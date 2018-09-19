13c5c60e4c9fe9022e723934a7d021371c9a3c62
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-09-19 14:58:58 +0200

    Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  pageTitle = 'Demo Component Interaction';
+  imgUrl = 'https://picsum.photos/101';
+  count = 0;
+  name: string;
+
+  incrementCount() {
+    this.count += 1;
+  }
 
 }
