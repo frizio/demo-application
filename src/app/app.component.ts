6362420d22be3f6105b8cbde3dd4a715df3ebdea
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-09-20 11:12:48 +0200

    reset all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,10 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
-import { ChildComponent } from './child/child.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  
-  userLoggedIn = true;
-
-  @ViewChild(ChildComponent)
-  childComponentRef: ChildComponent;
-
-  ngAfterViewInit(): void {
-    this.childComponentRef.message = 'Message from parent component';
-  }
-
+export class AppComponent {
+  title = 'My Application';
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,32 +1,15 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnChanges {
-
-  @Input()
-  loggedIn: boolean;
-  message: string;
-  name = 'Frizio';
+export class ChildComponent implements OnInit {
 
   constructor() { }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    // Useful when we have multiple input property to keep track
-    console.log(changes);
-    const loggedInValue = changes['loggedIn'];
-    if (loggedInValue.currentValue === true) {
-      this.message = 'Welcome back';
-    } else {
-      this.message = 'Please log in';
-    }
-  }
-
-  greetFrizio() {
-    alert('Hello Frizio!!!');
+  ngOnInit() {
   }
 
 }
