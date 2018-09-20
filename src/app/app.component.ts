905576ea9fd80602c17f2b168936427329247286
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-09-20 09:09:19 +0200

    6 - ViewChild Decorator + Refactor descriptions in page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+ 
   pageTitle = 'Demo Component Interaction';
   imgUrl = 'https://picsum.photos/101';
   count = 0;
   name: string;
   username: string;
   private _role: string;
+  @ViewChild('nameRef')
+  nameEleRef: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.nameEleRef.nativeElement.focus();
+    console.log(this.nameEleRef);
+  }
 
   get role(): string {
     return this._role;
