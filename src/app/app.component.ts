6f5acea3d5515709ec8da32f1ab3d448a9f5f2f4
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-09-20 11:29:18 +0200

    first step -  emit event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My Application';
+
+  greet() {
+    alert('Hello Frizio!!!');
+  }
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,9 +7,16 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 
+  @Output()
+  greetEvent = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  callParentGreet() {
+    this.greetEvent.emit();
   }
 
 }
