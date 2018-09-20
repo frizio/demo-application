c0737fc9633a2c7d26556cbbdbdd75875e4c3506
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-09-20 11:10:30 +0200

    12 - ViewChild Decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   
   userLoggedIn = true;
+
+  @ViewChild(ChildComponent)
+  childComponentRef: ChildComponent;
+
+  ngAfterViewInit(): void {
+    this.childComponentRef.message = 'Message from parent component';
+  }
 
 }
