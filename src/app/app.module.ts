fa9d7eae98eaf3fd355d5dc37d70b971469a322f
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-10-01 12:21:30 +0200

    Routes configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { DepartmentListComponent } from './department-list/department-list.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DepartmentListComponent,
-    EmployeeListComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
