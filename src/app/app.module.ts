bf47f236d1727a07dda2f13e00a76312a630cb50
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-10-01 12:14:01 +0200

    Generate the 2 new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentListComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
