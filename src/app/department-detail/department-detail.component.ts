edb574e507988a68c02d597dc3db87151b2297a3
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-10-01 15:05:30 +0200

    Problem with snapshor approach

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
@@ -11,12 +11,23 @@ export class DepartmentDetailComponent implements OnInit {
   public departmentId: number;
 
   constructor(
+    private router: Router,
     private _route: ActivatedRoute
   ) { }
 
   ngOnInit() {
     let id = parseInt(this._route.snapshot.paramMap.get("id"));
     this.departmentId = id
+  }
+
+  goBack() {
+    let id = this.departmentId + 1;
+    this.router.navigate(["/departments", id]);
+  }
+
+  goNext() {
+    let id = this.departmentId + 1;
+    this.router.navigate(["/departments", id]);
   }
 
 }
